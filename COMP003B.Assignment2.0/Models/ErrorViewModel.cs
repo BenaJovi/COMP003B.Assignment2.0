758519eb73f6307963bbdc992a028fcd2fde9ea3
Jovani Benavides <xavibenavides@yahoo.com>
2023-03-27 18:32:41 -0700

    Add project files.

---
+++ COMP003B.Assignment2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.Assignment2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
